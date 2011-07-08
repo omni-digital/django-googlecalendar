ebaeeecd453d8e558d2961190c7f0fd81c03f07e
George Hickman <george@incuna.com>
2011-07-08 12:18:25 +0100

    rename passed in variable to slug

--- googlecalendar/urls.py
+++ googlecalendar/urls.py
@@ -1,8 +1,8 @@
-from django.conf import settings
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('googlecalendar.views',
-    url(r'^(?P<calendar>[a-z0-9_-]+)/(?P<event>[a-z0-9_-]+)$', 'googlecalendar_event', name="googlecalendar_event"),
-    url(r'^(?P<calendar>[a-z0-9_-]+)/$', 'googlecalendar', name='googlecalendar_detail'),
+    url(r'^(?P<slug>[a-z0-9_-]+)/(?P<event>[a-z0-9_-]+)$', 'googlecalendar_event', name='googlecalendar_event'),
+    url(r'^(?P<slug>[a-z0-9_-]+)/$', 'googlecalendar', name='googlecalendar_detail'),
     url(r'^$', 'googlecalendar_list', name='googlecalendar'),
 )
+
