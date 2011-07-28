52c015ca2c68da9757df9dc1bea56e0898e95a3b
Marc Tamlyn <marc@incuna.com>
2011-07-28 17:27:03 +0100

    Removed strange requirements in setup.py

--- googlecalendar/__init__.py
+++ googlecalendar/__init__.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 
 if not hasattr(settings, 'USER_ADD_EVENTS'):
-    settings.USER_ADD_EVENTS = False__version__ = (1, 2, 1)
+    settings.USER_ADD_EVENTS = False
+
+__version__ = (1, 2)
 def get_version():
     return '.'.join(map(str, __version__))
--- setup.py
+++ setup.py
@@ -1,28 +1,8 @@
 from setuptools import setup, find_packages
-from setuptools.dist import Distribution
-import pkg_resources
 
-
-add_django_dependency = True
-# See issues #50, #57 and #58 for why this is necessary
-try:
-    pkg_resources.get_distribution('Django')
-    add_django_dependency = False
-except pkg_resources.DistributionNotFound:
-    try:
-        import django
-        if django.VERSION[0] >= 1 and django.VERSION[1] >= 1 and django.VERSION[2] >= 1:
-            add_django_dependency = False
-    except ImportError:
-        pass
-
-Distribution({
-    "setup_requires": add_django_dependency and  ['Django >=1.1.1'] or []
-})
-
-from $python_name import get_version
+from googlecalendar import get_version
 setup(
-    name = "googlecalendar",
+    name = "django-googlecalendar",
     packages = find_packages(),
     install_requires=[
         "gdata",
