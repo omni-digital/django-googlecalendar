832f93c9e570f8172e59b13165cd5324f18604a9
mitch104 <mitch_10_4@hotmail.com>
2018-07-18 10:33:32 +0100

    Do not use string imports

--- googlecalendar/urls.py
+++ googlecalendar/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, url
 
-urlpatterns = patterns('googlecalendar.views',
-    url(r'^(?P<slug>[a-z0-9_-]+)/(?P<event>[a-z0-9_-]+)$', 'googlecalendar_event', name='googlecalendar_event'),
-    url(r'^(?P<slug>[a-z0-9_-]+)/$', 'googlecalendar', name='googlecalendar_detail'),
-    url(r'^$', 'googlecalendar_list', name='googlecalendar'),
-)
+import googlecalendar.views as gc_views
 
+urlpatterns = [
+    url(r'^(?P<slug>[a-z0-9_-]+)/(?P<event>[a-z0-9_-]+)$', gc_views.googlecalendar_event, name='googlecalendar_event'),
+    url(r'^(?P<slug>[a-z0-9_-]+)/$', gc_views.googlecalendar, name='googlecalendar_detail'),
+    url(r'^$', gc_views.googlecalendar_list, name='googlecalendar'),
+]
